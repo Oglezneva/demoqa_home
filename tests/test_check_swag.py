0d1a1bae2421d6853793e10778e64cbb1ba1389a
Oglezneva <oglezneva@yahoo.com>
2023-05-03 13:42:38 +0300

    first commit

--- tests/test_check_swag.py
+++ tests/test_check_swag.py
@@ -1,19 +1,10 @@
 from pages.swag_labs import SwagLabs
 
 
-def test_check_icon(browser):
+def test_check_elements(browser):
     swag_labs = SwagLabs(browser)
     swag_labs.visit()
     assert swag_labs.exist_icon()
-
-
-def test_check_input_name(browser):
-    swag_labs = SwagLabs(browser)
-    swag_labs.visit()
     assert swag_labs.exist_input_name()
-
-
-def test_check_input_password(browser):
-    swag_labs = SwagLabs(browser)
-    swag_labs.visit()
     assert swag_labs.exist_input_password()
+
